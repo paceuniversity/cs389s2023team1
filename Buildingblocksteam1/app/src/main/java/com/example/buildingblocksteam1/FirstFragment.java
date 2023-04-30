dd203bffa189315e209020fbf18916c7650e2fa6
JParadiso01 <josephjpara@gmail.com>
2023-04-30 19:06:01 -0400

    added notifications and notifications with username

--- Buildingblocksteam1/app/src/main/java/com/example/buildingblocksteam1/FirstFragment.java
+++ Buildingblocksteam1/app/src/main/java/com/example/buildingblocksteam1/FirstFragment.java
@@ -5,9 +5,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -15,16 +15,19 @@ import com.example.buildingblocksteam1.databinding.FragmentFirstBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
+import java.util.Map;
 
 public class FirstFragment extends Fragment {
 
     private static final String TAG = "FirstFragment";
     private FragmentFirstBinding binding;
-
+    private String name = null;
     private FirebaseAuth mAuth;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -35,7 +38,6 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-
         return binding.getRoot();
 
     }
@@ -75,8 +77,7 @@ public class FirstFragment extends Fragment {
                 if (mAuth.getCurrentUser() == null) {
                     NavHostFragment.findNavController(FirstFragment.this)
                             .navigate(R.id.action_FirstFragment_to_signup);
-                }
-                else{
+                } else {
                     mAuth.signOut();
                 }
                 SigninOrSignout();
@@ -88,28 +89,26 @@ public class FirstFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
         SigninOrSignout();
+        notificationWithUsername();
     }
 
-    private void SigninOrSignout(){
-        if (mAuth.getCurrentUser() != null){
+    //checks whether user is signed in or not and updates UI
+    private void SigninOrSignout() {
+        if (mAuth.getCurrentUser() != null) {
             binding.buttonSignup.setText("Sign Out");
             binding.buttonSignup.invalidate();
-        }
-        else{
+        } else {
             binding.buttonSignup.setText("Sign In");
             binding.buttonSignup.invalidate();
         }
     }
 
-    //Gets Username from the user
-    //data is in document.getData() in the for loop log message
-
-    private void grabUsername(){
+    //Puts username in a global variable named "name"
+    private void grabUsername() {
         if (mAuth.getCurrentUser() != null) {
             String email = mAuth.getCurrentUser().getEmail();
-            db.collection("UserInformation")
+            Task<QuerySnapshot> result = db.collection("UserInformation")
                     .whereEqualTo("Email", email)
                     .get()
                     .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -117,15 +116,53 @@ public class FirstFragment extends Fragment {
                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
                             if (task.isSuccessful()) {
                                 for (QueryDocumentSnapshot document : task.getResult()) {
+                                    name = document.getData().get("username").toString();
                                     Log.d(TAG, document.getId() + " => " + document.getData());
+                                    Log.d(TAG, "name => " + name);
                                 }
                             } else {
                                 Log.d(TAG, "Error getting documents: ", task.getException());
                             }
                         }
                     });
-
         }
+    }
+
+    //creates a notification with the username
+    private void notificationWithUsername(){
+        if (mAuth.getCurrentUser() != null) {
+            String email = mAuth.getCurrentUser().getEmail();
+            Task<QuerySnapshot> result = db.collection("UserInformation")
+                    .whereEqualTo("Email", email)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    name = document.getData().get("username").toString();
+                                    Log.d(TAG, "name => " + name);
+                                    NotificationHelper notifyHelp = new NotificationHelper(getContext());
+                                    notifyHelp.createNotificationChannel();
+                                    String content = "Hello " + name + "!";
+                                    NotificationCompat.Builder builder = notifyHelp.setNotificationContent("Reminder",content);
+                                    notifyHelp.showNotification(1,builder);
+                                }
+                            } else {
+                                Log.d(TAG, "Error getting documents: ", task.getException());
+                            }
+                        }
+                    });
+        }
+    }
+
+    //creates a notification
+    private void NotificationTest(){
+        NotificationHelper notifyHelp = new NotificationHelper(getContext());
+        notifyHelp.createNotificationChannel();
+        String content = "Hello!";
+        NotificationCompat.Builder builder = notifyHelp.setNotificationContent("Reminder",content);
+        notifyHelp.showNotification(1,builder);
     }
     @Override
     public void onDestroyView() {
