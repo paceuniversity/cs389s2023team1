e84be0ccb5e5bb1dd68a471a5b2c975871f36bff
aidskiy <miaida96@gmail.com>
2023-05-02 18:35:34 -0400

    Created Lessons for Data Structures

---
+++ Buildingblocksteam1/app/src/main/java/com/example/buildingblocksteam1/HashTablesFragment.java
@@ -0,0 +1,85 @@
+package com.example.buildingblocksteam1;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.buildingblocksteam1.databinding.FragmentHashTablesBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HashTablesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HashTablesFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private FragmentHashTablesBinding binding;
+
+    public HashTablesFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment HashTablesFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HashTablesFragment newInstance(String param1, String param2) {
+        HashTablesFragment fragment = new HashTablesFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentHashTablesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(HashTablesFragment.this)
+                        .navigate(R.id.action_hashTablesFragment_to_data_structure_activity2);
+            }
+        });
+
+
+    }
+}
